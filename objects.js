17f2f9eee880c61a3c400e4aa512778bddcf50a2
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-19 12:54:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,8 +6,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
+function removeFromPlaylist(playlist, artistName) {
+  playlist[artistName].delete
   
   return playlist
 }
